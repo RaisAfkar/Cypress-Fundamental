eec5385422944bf7fe0030203d502d2ccc12a626
RaisAfkar <afkarrais27@gmail.com>
2024-09-30 15:50:10 +0700

    test

--- cypress/e2e/0.1-first-testing/first.cy.js
+++ cypress/e2e/0.1-first-testing/first.cy.js
@@ -3,8 +3,8 @@
 describe('My First Test', () => {
     it('Visit the Kitchen Sink', () => {
         cy.visit('https://example.cypress.io/')
-        cy.get('h1').contains('Kitchen Sink')
-        cy.contains('get').click()
-        cy.url().should('include', '/commands/querying')
+        // cy.get('h1').contains('Kitchen Sink')
+        // cy.contains('get').click()
+        // cy.url().should('include', '/commands/querying')
     });
 });
